674dea51598b5ed909ac613dfa244a56fc6640f7
njmorr <73321927+njmorr@users.noreply.github.com>
2021-04-22 11:34:33 -0500

    button functionality

--- static/js/app.js
+++ static/js/app.js
@@ -5,15 +5,37 @@ var tableData = data;
 
 
 // select the button
-var button = d3.select("#btn")
+var button = d3.select("#filter-btn")
 
 // select the form
-var filterForm = d3.select("#filters")
+var filterForm = d3.select("#form-control")
 
 // create the event handlers
-button.on("click", runFilter)
-filterForm.on("submit", runFilter)
+button.on("click", runFilter);
+filterForm.on("submit", runFilter);
+
+
+
+
+
+
 
 function runFilter() {
-    d3.event.preventDefault();
+    // d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+    var inputDate = inputElement.property("value");
+    
+    console.log(inputDate);
+    
+    function selectDate(ufoEvents) {
+        return ufoEvents.datetime === inputDate;
+    }
+
+    console.log(tableData.filter(selectDate));
+    // console.log(tableData.filter(ufoEvents => ufoEvents.datetime === inputeDate));
+
 }
+
+
+

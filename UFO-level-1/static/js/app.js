11bc63f1bf5f104e476096f6690f45e6668fd217
njmorr <73321927+njmorr@users.noreply.github.com>
2021-04-22 12:30:42 -0500

    if then handled

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -23,16 +23,29 @@ filterForm.on("submit", runFilter);
 function runFilter() {
     // d3.event.preventDefault();
 
+    // capture the datetime class as the input element and store that 'property value' as the inputDate variable
     var inputElement = d3.select("#datetime");
     var inputDate = inputElement.property("value");
     
+    // make sure date is captured as the variable correctly
     console.log(inputDate);
+   
     
+    // create function to return values of filter
     function selectDate(ufoEvents) {
         return ufoEvents.datetime === inputDate;
     }
+    // declare the results as a new variable
+    var returnedResults = tableData.filter(selectDate);
 
-    console.log(tableData.filter(selectDate));
+    // if the returnedResults variable is a non-empty array, show filtered results
+    // if teh returnedResults variable is an empty array, show all the data
+    if (returnedResults.length > 0) {
+        console.log(returnedResults);
+    } else {
+        console.log(tableData)
+    }
+   
     // console.log(tableData.filter(ufoEvents => ufoEvents.datetime === inputeDate));
 
 }

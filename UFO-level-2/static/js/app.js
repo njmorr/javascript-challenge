50381c28a165c80ed8950713a70502d7615f4501
njmorr <73321927+njmorr@users.noreply.github.com>
2021-04-22 13:08:05 -0500

    updated app.js

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -8,31 +8,62 @@ var tableData = data;
 var button = d3.select("#filter-btn")
 
 // select the form
-var filterForm = d3.select("#form-control")
+var filterForm = d3.select(".form-group")
 
 // create the event handlers
 button.on("click", runFilter);
 filterForm.on("submit", runFilter);
 
-
-
-
+// create reference to teh tbody section
+var tbody = d3.select("tbody")
 
 
 
 function runFilter() {
     // d3.event.preventDefault();
 
+    // capture the datetime class as the input element and store that 'property value' as the inputDate variable
     var inputElement = d3.select("#datetime");
     var inputDate = inputElement.property("value");
     
+    // make sure date is captured as the variable correctly
     console.log(inputDate);
+   
     
+    // create function to return values of filter
     function selectDate(ufoEvents) {
         return ufoEvents.datetime === inputDate;
     }
+    // declare the results as a new variable
+    var returnedResults = tableData.filter(selectDate);
 
-    console.log(tableData.filter(selectDate));
+    // if the returnedResults variable is a non-empty array, show filtered results
+    // if the returnedResults variable is an empty array, show all the data
+    if (returnedResults.length > 0) {
+        console.log(returnedResults);
+
+        returnedResults.forEach(function(ufoResults) {
+            var row = tbody.append("tr");
+            Object.entries(ufoResults).forEach(function([key,value]) {
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        });
+
+    } else {
+        console.log(tableData);
+
+        tableData.forEach(function(ufoResults) {
+            var row = tbody.append("tr");
+            Object.entries(ufoResults).forEach(function([key,value]) {
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        });
+    
+    }
+   
+    // alternate way of creating the filter function and displaying the results.
     // console.log(tableData.filter(ufoEvents => ufoEvents.datetime === inputeDate));
 
 }
